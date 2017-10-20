f9bb5847daf6a94a35a6b1e897a760c426822379
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-10-20 18:44:33 -0400

    Update giphy.js

--- js/giphy.js
+++ js/giphy.js
@@ -1,58 +1,71 @@
 $(document).ready(function() {
+	// ajax request steps -> $.get("url", callback function)
+	const apikey = 'bbj3km0ZEM6Ed9udFgpjnzIao3hYtkCy';
+	let xhrTrending = `http://api.giphy.com/v1/gifs/trending?api_key=${apikey}`;
+	let searchTerm = '';
+	let buttonText = '';
+	let offset = 0;
+	// sample url = http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5
 
-  // 1- Get trending photos when html has finished loading
-  var xhrTrending = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&tag=59b72bd8344c42ec8faea4fd13e6318d&limit=49";
-  var xhrSearch = "";
+	function displayPhotos(data) {
+		var divID = '';
+		$.each(data.data, function(i, photo) {
+			var photoHTML = "<div class='tile is-child'>";
+			photoHTML += '<a href="' + photo.url + '" class="image" target="_blank">';
+			photoHTML += '<img src="' + photo.images.fixed_width.url + '"></a>';
+			photoHTML += '</div>';
+			switch (i % 4) {
+				case 0:
+					divID = '#col_0';
+					break;
+				case 1:
+					divID = '#col_1';
+					break;
+				case 2:
+					divID = '#col_2';
+					break;
+				case 3:
+					divID = '#col_3';
+					break;
+			}
+			$(divID).append(photoHTML);
+		});
+	}
 
-  function displayPhotos (data) {
-    var divID = '';
-    $.each(data.data, function(i, photo) {
-      var photoHTML = "<div class='tile is-child'>";
-      photoHTML += '<a href="' + photo.url + '" class="image" target="_blank">';
-      photoHTML += '<img src="' + photo.images.fixed_width.url + '"></a>';
-      photoHTML += '</div>';
-      switch (i % 4) {
-        case 0:
-          divID = '#col_0';
-          break;
-        case 1:
-          divID = '#col_1';
-          break;
-        case 2:
-          divID = '#col_2';
-          break;
-        case 3:
-          divID = '#col_3';
-          break;
-      }
-      $(divID).append(photoHTML);
-    });
-  };
-  $.getJSON(xhrTrending, displayPhotos);
+	$.getJSON(xhrTrending, displayPhotos);
 
+	function xhrSearchTerm(searchTerm) {
+		return `http://api.giphy.com/v1/gifs/search?q=${searchTerm.toLowerCase()}&api_key=${apikey}`;
+	}
+	function clearCache() {
+		offset = 0;
+		$('#col_0, #col_1, #col_2, #col_3').empty();
+	}
 
-  // 2- Search what user inputs
-  $('.search_button').click(function() {
-    /* Act on the event */
-    $('#col_0, #col_1, #col_2, #col_3').empty();
-    if ($('input').val() != "") {
-      xhrSearch = "http://api.giphy.com/v1/gifs/search?q=" + encodeURI($('input').val()) + "&api_key=dc6zaTOxFJmzC&tag=59b72bd8344c42ec8faea4fd13e6318d&limit=49"
-      $.getJSON(xhrSearch, displayPhotos);
-    } else {
-      $.getJSON(xhrTrending, displayPhotos);
-    }
-  });
+	// get photos from search results
+	$('.giphy_search').submit(function(event) {
+		event.preventDefault();
+		clearCache();
+		// get the text of the input tag
+		searchTerm = $('.input').val();
+		if (searchTerm != '') {
+			// make ajax request
+			$.get(xhrSearchTerm(searchTerm), displayPhotos);
+		} else {
+			$.getJSON(xhrTrending, displayPhotos);
+		}
+		return false;
+	});
 
-
-  // 3- When user clicks see more button, load more photos...
-  $('.see_more').click(function() {
-    /* Act on the event */
-    if ($('input').val() == "") {
-      // User still in trending page
-      $.getJSON(xhrTrending, displayPhotos);
-    } else {
-      // User has searched for something
-      $.getJSON(xhrSearch, displayPhotos);
-    }
-  });
+	// load more gifs
+	$('.see_more').on('click', function(event) {
+		event.preventDefault();
+		let xhrTempSearchTerm = '';
+		searchTerm != ''
+			? (xhrTempSearchTerm = `${xhrSearchTerm(
+					searchTerm
+				)}&offset=${(offset += 1)}`)
+			: (xhrTempSearchTerm = `${xhrTrending}&offset=${(offset += 1)}`);
+		$.get(xhrTempSearchTerm, displayPhotos);
+	});
 });
